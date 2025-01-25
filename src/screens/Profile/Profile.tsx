4a824de1a3f67f2f7c5d971636075b75b250dd92
Dipankar Giri <giridipankar@Dipankars-MacBook-Pro.local>
2025-01-25 22:30:03 +0530

    feature: changes in profile page & bottom nav/ header icons

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -3,31 +3,35 @@ import React from 'react';
 import {tws} from '../../utility/tailwind';
 import AppText from '../../components/AppText';
 import {AppIcon} from '../../components/AppIcon';
+import {Colors} from '../../constants/Colors';
 
 export default function Profile() {
   return (
-    <View style={tws('flex-1 bg-white px-4 py-4')}>
-      <AppText size={18} bold>
+    <View style={tws('flex-1 bg-white')}>
+      {/* <AppText size={18} bold>
         Profile
-      </AppText>
-      <View
-        style={tws(
-          'flex-row gap-4 mt-4 justify-between border-b border-b-gray-200 pt-2 pb-4',
-        )}>
-        <View style={tws('flex-row gap-4')}>
-          <View style={tws('w-12 h-12 bg-red-200 rounded-full items-center')} />
+      </AppText> */}
+      {/* <View style={{}}> */}
+      <View style={tws(`w-full bg-gray-300 border-1 h-36 border-gray-200`)}>
+        <View style={tws('absolute bottom-[-18] ml-4')}>
+          <View
+            style={tws(
+              'w-18 h-18 bg-red-200 rounded-full items-center',
+            )}></View>
           <View style={tws('')}>
             <AppText size={16}>Harsh</AppText>
             <AppText size={14} color="gray">
               9876543211
             </AppText>
           </View>
-        </View>
-        <TouchableOpacity>
+          {/* <TouchableOpacity>
           <AppIcon type="MaterialIcons" name="edit" size={20} color="gray" />
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+        </View>
       </View>
-      <View>
+      {/* </View> */}
+      <View style={tws()}></View>
+      {/* <View>
         <TouchableOpacity style={tws('flex-row items-center gap-4 py-4 px-2')}>
           <AppIcon type="MaterialIcons" name="qr-code-scanner" size={30} />
           <AppText size={14}>Scan Code</AppText>
@@ -40,7 +44,7 @@ export default function Profile() {
           <AppIcon type="MaterialIcons" name="star" size={30} />
           <AppText size={14}>Rate Us</AppText>
         </TouchableOpacity>
-      </View>
+      </View> */}
     </View>
   );
 }
